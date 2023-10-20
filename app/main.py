937a77cf667f33f170e103bd5bc9b193ed9e62d8
Mikhail Drozdov <deathlokmike@gmail.com>
2023-10-20 16:22:47 +0300

    Rebuild frontend

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.controllers.pages import router as router_view
 from app.controllers.pneumonia import router as router_pneumonia
 from app.controllers.auth import router as router_auth
 
-app = FastAPI()
+app = FastAPI(title="Clinic API")
 app.mount("/static", StaticFiles(directory="app/views/static"), name="static")
 app.include_router(router_view)
 app.include_router(router_auth)
--- app/views/static/scripts/registration.js
+++ app/views/static/scripts/registration.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const passwordInput = document.getElementById('password_field');
-    const repeatPasswordInput = document.getElementById('repeat_password_field');
+    const passwordInput = document.getElementById('password');
     const togglePasswordButton = document.getElementById('toggle_password');
     const hiddenIcon = document.querySelector('.toggle-password .hidden');
     const shownIcon = document.querySelector('.toggle-password img:not(.hidden)');
@@ -8,12 +7,10 @@ document.addEventListener('DOMContentLoaded', function () {
     togglePasswordButton.addEventListener('click', function () {
         if (passwordInput.type === 'password') {
             passwordInput.type = 'text';
-            repeatPasswordInput.type = 'text';
             shownIcon.classList.add('hidden');
             hiddenIcon.classList.remove('hidden');
         } else {
             passwordInput.type = 'password';
-            repeatPasswordInput.type = 'password';
             hiddenIcon.classList.add('hidden');
             shownIcon.classList.remove('hidden');
         }
